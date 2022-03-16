c649508292abea0ce536edb8f472bb9031ccc3a2
Haotian Mo <toreimimo@gmail.com>
2022-03-15 21:39:16 -0400

    Upload initial project

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float speed = 10;
+
+    Void Update() 
+    {
+        
+    }
+}
